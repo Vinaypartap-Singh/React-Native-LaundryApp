0ff8cbc8c74540033db62113e41923531a18e7f6
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-09 21:22:48 +0530

    Navigation Added

---
+++ StackNavigation.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import Pickup from "./screens/Pickup";
+
+export default function StackNavigation() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Pickup"
+          component={Pickup}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
