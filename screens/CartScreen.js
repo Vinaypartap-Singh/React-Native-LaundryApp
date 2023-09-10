ed9fba73da8ee9ffa8fe93d8cb71515df90ab34a
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-10 11:54:08 +0530

    Cart Home Screen Added

---
+++ screens/CartScreen.js
@@ -0,0 +1,328 @@
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { decreaseQuantity, increaseQuantity } from "../store/cartSlice";
+import { decrementQty, increamentQty } from "../store/productSlice";
+
+export default function CartScreen() {
+  const route = useRoute();
+  console.log(route.params);
+  const addressdata = route.params;
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart
+    .map((item) => item.quantity * item.price)
+    .reduce((curr, prev) => curr + prev, 0);
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      {total === 0 ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            height: "auto",
+            paddingVertical: 50,
+          }}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+            Your Cart is Empty
+          </Text>
+          <View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Home")}
+              style={{
+                borderWidth: 1,
+                paddingHorizontal: 40,
+                paddingVertical: 15,
+                borderRadius: 10,
+                marginTop: 30,
+              }}
+            >
+              <Text>Continue Shopping</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <View style={{ paddingHorizontal: 20 }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+            <Text style={{ marginLeft: 10 }}>Your Bucket</Text>
+          </View>
+          <View>
+            {cart.map((data, index) => {
+              return (
+                <View key={index} style={{ marginTop: 20 }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      backgroundColor: "white",
+                      paddingHorizontal: 20,
+                      paddingVertical: 25,
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                      {data.name}
+                    </Text>
+                    {/* Button & Quantity */}
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                      }}
+                    >
+                      <TouchableOpacity
+                        onPress={() => {
+                          dispatch(decreaseQuantity(data));
+                          dispatch(decrementQty(data));
+                        }}
+                        style={{
+                          borderWidth: 1,
+                          paddingHorizontal: 8,
+                          paddingVertical: 4,
+                          borderRadius: 50,
+                        }}
+                      >
+                        <Text>-</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity>
+                        <Text>{data.quantity}</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        onPress={() => {
+                          dispatch(increaseQuantity(data));
+                          dispatch(increamentQty(data));
+                        }}
+                        style={{
+                          borderWidth: 1,
+                          paddingHorizontal: 8,
+                          paddingVertical: 4,
+                          borderRadius: 50,
+                        }}
+                      >
+                        <Text>+</Text>
+                      </TouchableOpacity>
+                    </View>
+                    {/* Total */}
+                    <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                      $ {data.price * data.quantity}
+                    </Text>
+                  </View>
+                </View>
+              );
+            })}
+            <View style={{ alignItems: "flex-start" }}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Home")}
+                style={{
+                  borderWidth: 1,
+                  paddingHorizontal: 40,
+                  paddingVertical: 15,
+                  borderRadius: 10,
+                  marginTop: 30,
+                }}
+              >
+                <Text style={{ textAlign: "center" }}>Shop More</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+                Billing Details
+              </Text>
+              <View
+                style={{
+                  backgroundColor: "white",
+                  paddingHorizontal: 20,
+                  paddingVertical: 20,
+                  marginTop: 20,
+                  borderRadius: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Item Total</Text>
+                  <Text style={{ fontSize: 16 }}> ${total} </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Delivery Fee</Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fb5c63",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Free
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>
+                    Free Delivery on your order
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    borderTopWidth: 1,
+                    borderRadius: 10,
+                    marginTop: 10,
+                    borderColor: "lightgrey",
+                  }}
+                ></View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Selected Date</Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fb5c63",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {addressdata.selectedDate}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Delivery Date</Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fb5c63",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {addressdata.selectedDelivery}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Pickup Date</Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fb5c63",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {addressdata.selectedPickupTime}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    marginTop: 10,
+                    borderTopWidth: 1,
+                    borderRadius: 10,
+                    borderColor: "lightgrey",
+                  }}
+                ></View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>Total Amount To Pay</Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fb5c63",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {total + 20}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                marginTop: "auto",
+                marginTop: 20,
+                flexDirection: "row",
+                alignItems: "flex-start",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#fb5c63",
+                  paddingHorizontal: 40,
+                  paddingVertical: 15,
+                  borderRadius: 10,
+                  shadowColor: "red",
+                  shadowOffset: {
+                    width: 0,
+                    height: 1,
+                  },
+                  shadowOpacity: 0.22,
+                  shadowRadius: 2.22,
+
+                  elevation: 3,
+                }}
+              >
+                <Text style={{ color: "white", fontWeight: "bold" }}>
+                  Pay Now
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,270 @@
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { MaterialIcons, Feather } from "@expo/vector-icons";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import DressItem from "../components/DressItem";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts } from "../store/productSlice";
+import { useNavigation } from "@react-navigation/native";
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart
+    .map((item) => item.quantity * item.price)
+    .reduce((curr, prev) => curr + prev, 0);
+  // console.log(cart);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState("Loading");
+  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
+
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      Alert.alert(
+        //title
+        "Location service not enabled",
+        //body
+        "Please Enable location service to procees",
+        [
+          { text: "Yes", onPress: () => console.log("Yes Pressed") },
+          {
+            text: "No",
+            onPress: () => console.log("No Pressed"),
+            style: "cancel",
+          },
+        ],
+        { cancelable: false }
+        //clicking out side of alert will not cancel
+      );
+    } else {
+      setLocationServiceEnabled(true);
+    }
+  };
+
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "allow the app to use the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+    // console.log(coords)
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      // console.log(response);
+
+      for (let item of response) {
+        let address = `${item.region} ${item.city} ${item.postalCode}`;
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
+
+  const product = useSelector((state) => state.product.product);
+  useEffect(() => {
+    if (product.length > 0) return;
+
+    const fetchProducts = () => {
+      dressServices.map((service) => dispatch(getProducts(service)));
+    };
+
+    fetchProducts();
+  }, []);
+
+  // console.log(product);
+
+  // DressItem Services
+
+  const dressServices = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "Shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "T-shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "Dresses",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "Jeans",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/9431/9431166.png",
+      name: "Sweater",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/3345/3345397.png",
+      name: "Shorts",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/293/293241.png",
+      name: "Sleeveless",
+      quantity: 0,
+      price: 10,
+    },
+  ];
+
+  return (
+    <>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        showsVerticalScrollIndicator={false}
+      >
+        {/* Location and Header Profile Icon */}
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: 10,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialIcons name="location-on" size={30} color="#fb5c63" />
+            <View>
+              <Text style={{ fontWeight: "bold", fontSize: 18 }}>Hello</Text>
+              <Text>{displayCurrentAddress}</Text>
+            </View>
+          </View>
+          <View>
+            <Image
+              width={50}
+              height={50}
+              style={{ borderRadius: 50 }}
+              source={{
+                uri: "https://lh3.googleusercontent.com/ogw/AAEL6sh_yqHq38z35QMy5Fnb8ZIxicdxCIVM9PeBD2j-=s64-c-mo",
+              }}
+            />
+          </View>
+        </View>
+        {/* Search Bar  */}
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 10,
+            marginVertical: 15,
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 10,
+            justifyContent: "space-between",
+            borderColor: "#C0C0C0",
+          }}
+        >
+          <TextInput
+            placeholder="Search for Items or More"
+            style={{ fontSize: 18 }}
+          />
+          <Feather name={"search"} size={28} color={"#fb5c63"} />
+        </View>
+        {/* Hero Image Component */}
+        <Hero />
+        {/* Services Component */}
+        <Services />
+        {total === 0 ? null : (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Cart")}
+            style={{
+              backgroundColor: "#088f8f",
+              marginHorizontal: 20,
+              borderRadius: 10,
+              padding: 10,
+              marginBottom: 10,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                paddingHorizontal: 10,
+                paddingVertical: 10,
+              }}
+            >
+              <View>
+                <Text
+                  style={{ color: "white", fontWeight: "bold", fontSize: 18 }}
+                >
+                  {cart.length} Items | ${total}
+                </Text>
+                <Text
+                  style={{ color: "white", fontWeight: "bold", fontSize: 14 }}
+                >
+                  extra charges might apply
+                </Text>
+              </View>
+              <View>
+                <TouchableOpacity onPress={() => navigation.navigate("Pickup")}>
+                  <Text>Proceed to Pickup</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+        {product.map((data, index) => {
+          return <DressItem data={data} key={index} />;
+        })}
+      </ScrollView>
+    </>
+  );
+}
