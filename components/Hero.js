4c4a3afd1f065ee67f96d93b8276ce430e1ce3ba
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-10-03 21:17:06 +0530

    Hero Section Update

--- components/Hero.js
+++ components/Hero.js
@@ -5,11 +5,10 @@ export default function Hero() {
   return (
     <View style={{ paddingHorizontal: 10 }}>
       <Image
-        style={{ borderRadius: 10 }}
+        style={{ borderRadius: 10, width: "100%", objectFit: "cover" }}
         height={250}
-        source={{
-          uri: "https://media.istockphoto.com/id/1247884083/vector/laundry-service-room-vector-illustration-washing-and-drying-machines-with-cleansers-on-shelf.jpg?s=612x612&w=0&k=20&c=myaNEKlqX7R--bzWGDoMI7PhdxG_zdQTKYEBlymJQGk=",
-        }}
+        resizeMode="cover"
+        source={require("../assets/images/heroImage.jpeg")}
       />
     </View>
   );
